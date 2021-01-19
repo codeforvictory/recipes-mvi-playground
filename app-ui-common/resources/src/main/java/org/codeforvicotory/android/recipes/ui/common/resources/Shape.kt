76d33ff1176993ed58b99c7f50f447ffb22c1a6f
Cesar Diez Sanchez <cesaryomismo@gmail.com>
2021-01-19 10:48:41 -0500

    Added Detekt + Spotless + Github workflow setup.

--- app-ui-common/resources/src/main/java/org/codeforvicotory/android/recipes/ui/common/resources/Shape.kt
+++ app-ui-common/resources/src/main/java/org/codeforvicotory/android/recipes/ui/common/resources/Shape.kt
@@ -1,8 +1,5 @@
 package org.codeforvicotory.android.recipes.ui.common.resources
 
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.unit.dp
-
 /*val shapes = Shapes(
   small = RoundedCornerShape(4.dp),
   medium = RoundedCornerShape(4.dp),
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,14 @@ buildscript {
   }
 }
 
+plugins {
+  // https://plugins.gradle.org/plugin/com.diffplug.spotless
+  id("com.diffplug.spotless") version "5.9.0" apply true
+  // https://plugins.gradle.org/plugin/io.gitlab.arturbosch.detekt
+  id("io.gitlab.arturbosch.detekt") version "1.15.0"
+}
+
+// Use allprojects API instead of subprojects for repository provision, since it also provides build's plugins block dependencies.
 allprojects {
   repositories {
     google()
@@ -16,6 +24,21 @@ allprojects {
   }
 }
 
-tasks.register("clean", Delete::class) {
-  delete(rootProject.buildDir)
+detekt {
+  config = files("$rootDir/ci/detekt/project-config.yml")
+  buildUponDefaultConfig = true
+}
+
+subprojects {
+  apply {
+    plugin("com.diffplug.spotless")
+    plugin("io.gitlab.arturbosch.detekt")
+  }
+
+  spotless {
+    kotlin {
+      target("**/src/**/*.kt", "**/src/**/*.kts")
+      ktlint("0.40.0")
+    }
+  }
 }
