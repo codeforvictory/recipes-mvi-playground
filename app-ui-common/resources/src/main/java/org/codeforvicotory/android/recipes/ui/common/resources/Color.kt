0dc4797074a853e1a46f9e3f928510a3ba4339ab
Cesar Diez Sanchez <cesaryomismo@gmail.com>
2021-01-20 23:44:09 -0500

    Detekt fixes.

--- app-ui-common/resources/src/main/java/org/codeforvicotory/android/recipes/ui/common/resources/Color.kt
+++ app-ui-common/resources/src/main/java/org/codeforvicotory/android/recipes/ui/common/resources/Color.kt
@@ -2,7 +2,13 @@ package org.codeforvicotory.android.recipes.ui.common.resources
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
-val teal200 = Color(0xFF03DAC5)
+private const val COLOR_PURPLE_200: Long = 0xFFBB86FC
+private const val COLOR_PURPLE_500: Long = 0xFF6200EE
+private const val COLOR_PURPLE_700: Long = 0xFF3700B3
+private const val COLOR_TEAL_200: Long = 0xFF03DAC5
+
+val purple200 = Color(COLOR_PURPLE_200)
+val purple500 = Color(COLOR_PURPLE_500)
+val purple700 = Color(COLOR_PURPLE_700)
+val teal200 = Color(COLOR_TEAL_200)
+
